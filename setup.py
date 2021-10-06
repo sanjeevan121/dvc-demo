086b979aa10f4cbbdf9253837ba773e3d29484cd
sanjeevan121 <sanjeevan.k.thorat@gmail.com>
2021-10-07 00:48:23 +0530

    added test

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+
+setup(name='src',
+    version='0.0.1',
+    description='mlops package',
+    author='sanjeevan thorat',packages=find_packages(),author_email='sanjeevan.k.thorat@gmail.com',license="MIT")
+    
